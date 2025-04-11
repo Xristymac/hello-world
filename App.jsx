106aba51ea56c8d59f617bc963b7bc5c042ade77
Xristymac <xristymac@gmail.com>
2025-04-11 07:33:23 +0100

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="container">
+      <h1>Hello World! 🎉</h1>
+      <p>Welcome to my first React app.</p>
+    </div>
+  );
+}
+
+export default App;
